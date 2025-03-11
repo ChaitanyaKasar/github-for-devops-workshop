b1f94653f35565a0f39ef3be4d78968900f2883e
Chaitanya Kasar <chaitanya.kasar@aress.com>
2025-03-11 11:16:00 +0530

    new changes has been made

--- demo.py
+++ demo.py
@@ -1,6 +1,15 @@
+"""
+This is a demo module.
+"""
+
+
 def my_function():
 
+    """
+    This function returns a greeting message.
+    """
     return "Hello World"
 
 
-my_function()
+if __name__ == "__main__":
+    print(my_function())
